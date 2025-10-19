ec0150431df8ce7de21c9675720f98f01c78c0ad
naanjum <nafishaanjum.30522@gmail.com>
2025-10-19 17:40:54 -0500

    Initial project setup: add new_app.py

---
+++ new_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta, date
+import plotly.graph_objects as go
+import yfinance as yf
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import requests
+import warnings
+
+# ... (rest of your new_app.py as provided above)
